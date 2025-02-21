93a878f5c6f6590c08056b2d7b01b34eb3d0f936
usamahussain1 <usama.hussain@xpertdigital.co>
2025-02-21 18:33:06 +0500

    Change title from metadata

--- src/app/(routes)/product/layout.tsx
+++ src/app/(routes)/product/layout.tsx
@@ -1,4 +1,10 @@
+import { Metadata } from 'next';
 import React from 'react'
+
+export const metadata: Metadata = {
+  title: "Products",
+  description: "Showing Products",
+};
 
 function layout({ children }: { children: React.ReactNode }) {
   return (
