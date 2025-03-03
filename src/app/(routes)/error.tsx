2becc431b14bbed91486c159017dd1cb2389e243
usamahussain1 <usama.hussain@xpertdigital.co>
2025-03-03 12:09:30 +0500

    reset error props

--- src/app/(routes)/error.tsx
+++ src/app/(routes)/error.tsx
@@ -1,11 +1,12 @@
 "use client"
 import React from 'react'
 
-function ErrorPage({error} : {error: Error}) {
+function ErrorPage({error, reset} : {error: Error, reset: () => void}) {
   return (
     <>
     <div>Error Found</div>
     <p>{error.message}</p>
+    <button onClick={()=>reset()}>Try Again</button>
     </>
   )
 }
